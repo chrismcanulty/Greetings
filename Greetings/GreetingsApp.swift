c5e94cb645ef3ee38077a7b8e2d4e8a8be7a5a13
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-09-23 14:49:40 -0700

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Chris McAnulty on 2023-09-23.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
