e40b4aa462546702d2819a03f6038904ecc0ff07
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-09-23 15:07:13 -0700

    first commit

--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            LinearGradient(colors: [.cyan, .blue, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .opacity(0.7)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                
+                Text("Greetings!")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .padding()
+                    .foregroundStyle(.red)
+                    .background(.orange)
+                    .cornerRadius(20)
+                    .shadow(color: .orange, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 5, y: 5)
+                    .padding()
+            }
         }
-        .padding()
     }
 }
 
