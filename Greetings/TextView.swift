2b47d81ffa88228eacb19eb593314bdbfb059c46
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-09-24 17:24:05 -0700

    text background color changes on tap gesture, with animation

--- Greetings/TextView.swift
+++ Greetings/TextView.swift
@@ -4,7 +4,20 @@ import SwiftUI
 struct TextView: View {
     
     let text: String
-    let color: Color
+    @State var color: Color
+    
+    let colors: [Color] = [
+        .red,
+        .green,
+        .blue,
+        .orange,
+        .yellow,
+        .purple,
+        Color(red: 0.5, green: 0, blue: 0.5),
+        Color(red: 0, green: 0.5, blue: 0.5),
+        Color(red: 139/255, green: 207/255, blue: 240/255),
+        Color(red: 1, green: 215/255, blue: 0),
+    ]
     
     var body: some View {
         Text(text)
@@ -14,6 +27,12 @@ struct TextView: View {
             .background(color .opacity(0.4))
             .cornerRadius(20)
             .shadow(color: color, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 10, y: 10)
+            .onTapGesture {
+                // randomly change color
+                withAnimation {
+                    color = colors.randomElement() ?? .red
+                }
+            }
     }
 }
 
