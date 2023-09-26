cc623e6434db5813d2d8830332acfe42bd540a5c
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-09-26 13:16:24 -0700

    created custom colours in assets folder with different pallet for dark and light mode and integrate into views

--- Greetings/BackgroundView.swift
+++ Greetings/BackgroundView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 
 struct BackgroundView: View {
     var body: some View {
-        LinearGradient(colors: [.blue, Color(red: 139 / 255, green: 80 / 255, blue: 240 / 255)], startPoint: .topLeading, endPoint: .bottomTrailing)
+        LinearGradient(colors: [.myBlue1, .myBlue2], startPoint: .topLeading, endPoint: .bottomTrailing)
             .opacity(0.3)
             .ignoresSafeArea()
     }
