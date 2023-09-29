773c70445e09bc174821b57ee5dfa570f1d79eb9
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-09-28 18:59:02 -0700

    detect whether device is ipad and use appropriate view and font size if so

--- Greetings/Views/MainView.swift
+++ Greetings/Views/MainView.swift
@@ -1,15 +1,24 @@
 
 import SwiftUI
 
-// Portrait = Compact width, regular height
+// iPhone Portrait = Compact width, regular height
+// iPad = Regular width, regular height
 
 struct MainView: View {
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
     @Environment(\.verticalSizeClass) var verticalSizeClass
     
+    var isPortraitPhone: Bool {
+        horizontalSizeClass == .compact && verticalSizeClass == .regular
+    }
+    
+    var isIPad: Bool {
+        horizontalSizeClass == .regular && verticalSizeClass == .regular
+    }
+    
     var body: some View {
         // Portrait mode?
-        if horizontalSizeClass == .compact && verticalSizeClass == .regular {
+        if isPortraitPhone || isIPad {
             GreetingsView()
         } else {
             // Landscape mode?
